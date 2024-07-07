2da7fffc2aadc0b97d32227d2189c20d0886bcf8
justincj <justincgeorge95@gmail.com>
2024-07-07 14:00:07 +0530

    opening todays file with vim by default

--- cnote.go
+++ cnote.go
@@ -1,31 +1,41 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "time"
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
 )
 
-
 func main() {
-    today := time.Now().Format("2006-01-02")
+	today := time.Now().Format("2006-01-02")
 
-    filename := today + ".md"
+	filename := today + ".md"
 
-    _, err := os.Stat(filename)
+	_, err := os.Stat(filename)
 
-    if os.IsNotExist(err) {
-        file, err := os.Create(filename)
-        if err != nil {
-            fmt.Printf("Error creating file: %v\n", err)
-            os.Exit(1);
-        }
-        defer file.Close()
-        fmt.Printf("created note: %s\n", filename);
-    } else if err != nil {
-        fmt.Printf("Error checking file %v\n", err)
-        os.Exit(1)
-    } else {
-        fmt.Printf("Note for today (%s) already exists\n", filename)
-    }
+	if os.IsNotExist(err) {
+		file, err := os.Create(filename)
+		if err != nil {
+			fmt.Printf("Error creating file: %v\n", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		fmt.Printf("created note: %s\n", filename)
+	} else if err != nil {
+		fmt.Printf("Error checking file %v\n", err)
+		os.Exit(1)
+	} else {
+		fmt.Printf("Note for today (%s) already exists\n", filename)
+	}
+
+	cmd := exec.Command("vim", filename)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("Error opening vim: %v\n", err)
+	}
 }
